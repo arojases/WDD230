2ff38a01ce994cdd584b38a72cfba96f68e8de42
arojases <arielalejandrorojas@gmail.com>
2020-09-18 03:56:52 -0600

    Update Assignment: Course Portal Home Page

---
+++ js/currentDate.js
@@ -0,0 +1,3 @@
+//toLocateDateString
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
