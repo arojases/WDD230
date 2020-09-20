af786555795418f710346714f23098c831282588
arojases <arielalejandrorojas@gmail.com>
2020-09-19 19:15:21 -0600

    LastModified

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,3 +1,6 @@
 //toLocateDateString
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+//const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+//document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+
+let lastMod = document.lastModified;
+document.getElementById('currentDate').textContent = lastMod;
